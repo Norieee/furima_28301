1458f500f964d1ffa1ea92a6cccb0608722b27c6
Norieee <aok76283@gmail.com>
2020-08-11 21:54:25 +0900

    テストコード準備

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.initials}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name {"あア阿"}
+    last_name {"あア阿"}
+    f_name_kana {"ア"}
+    l_name_kana {"ア"}
+    birthday {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
+  end
+end

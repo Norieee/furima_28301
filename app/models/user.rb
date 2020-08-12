9384b7c9d07e3278f2d05d316038a4a4cfb9ce37
Norieee <aok76283@gmail.com>
2020-08-12 17:04:08 +0900

    繰り返す正規表現の変数代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,19 @@ class User < ApplicationRecord
   has_many :items
   has_many :ordered_items
 
+  japanese_letters = /\A[ぁ-んァ-ン一-龥]+\z/
+  japanese_kana_letters = /\A[ァ-ンー]+\z/
+
   with_options presence: true do
     validates :nickname, uniqueness: { case_sensitive: true }
     validates :email, uniqueness: { case_sensitive: true },
                       format: { with: /\w+([-+.]\w+)*@\w+([-.]\w+)*/ }
-    validates :password, format: { with: /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+\z/, message: 'includes both letters and numbers' }
+    validates :password, format: { with: /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+\z/, message: 'includes both half-width letters and half-width numbers' }
     validates :password_confirmation
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is not full-width characters' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is not full-width characters' }
-    validates :f_name_kana, format: { with: /\A[ァ-ンー]+\z/, message: 'is not full-width katakana characters' }
-    validates :l_name_kana, format: { with: /\A[ァ-ンー]+\z/, message: 'is not full-width katakana characters' }
+    validates :first_name, format: { with: japanese_letters, message: 'is not full-width characters' }
+    validates :last_name, format: { with: japanese_letters, message: 'is not full-width characters' }
+    validates :f_name_kana, format: { with: japanese_kana_letters, message: 'is not full-width katakana characters' }
+    validates :l_name_kana, format: { with: japanese_kana_letters, message: 'is not full-width katakana characters' }
     validates :birthday
   end
 end

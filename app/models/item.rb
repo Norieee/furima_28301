52aa238c1f398010ee829e24119cb287e85a557f
Norieee <aok76283@gmail.com>
2020-08-13 12:54:23 +0900

    出品機能実装（フォームから送信されたデータの保存）

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,14 +4,17 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :status, :shipping_charges,
-                         :prefecture, :days_until_shipping
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :days_until_shipping
 
   #空の投稿を保存できないようにする
-  validates :name, :explanation, :category, :status, :shipping_charges,
-            :prefecture, :days_until_shipping, presence: true
+  validates :name, :explanation, :category_id, :status_id, :shipping_charges_id,
+            :prefecture_id, :days_until_shipping_id, presence: true
 
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id, :status_id, :shipping_charges_id,
-            :prefecture, :days_until_shipping, numericality: { other_than: 1 } 
+            :prefecture_id, :days_until_shipping_id, numericality: { other_than: 1 } 
 end

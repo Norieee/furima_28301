414ad4f29265f789705df3c590b785af31cc7404
Norieee <aok76283@gmail.com>
2020-08-14 15:53:22 +0900

    Validation設定追加・エラー文の表示設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,14 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :days_until_shipping
 
-  #空の投稿を保存できないようにする
-  validates :name, :explanation, :category_id, :status_id, :shipping_charges_id,
-            :prefecture_id, :days_until_shipping_id, presence: true
-
-  #ジャンルの選択が「--」の時は保存できないようにする
-  validates :category_id, :status_id, :shipping_charges_id,
-            :prefecture_id, :days_until_shipping_id, numericality: { other_than: 1 } 
+  with_options presence: true do
+    validates :image, :name, :explanation
+    validates :price,
+              numericality: { with: /\A[0-9]+\z/, message: 'is not half-width numbers' },
+              numericality: { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999,
+                              message: 'is out of the setting range' }
+    validates :category_id, :status_id, :shipping_charges_id,
+              :prefecture_id, :days_until_shipping_id,
+              numericality: { other_than: 1, message: 'must select' } 
+  end
 end

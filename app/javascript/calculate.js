0c0da4a9104922cd29a85839633494d83a22f541
Norieee <aok76283@gmail.com>
2020-08-14 11:48:32 +0900

    非同期的な金額表示（手数料・利益）

---
+++ app/javascript/calculate.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function(){
+  const price = document.getElementById('item-price');
+  const addTaxPrice = document.getElementById('add-tax-price');
+  const profit = document.getElementById('profit');
+
+  price.addEventListener('input', (e) => {
+    const innerPrice = price.value;
+    console.log(innerPrice);
+    const tax = innerPrice / 10;
+    const pro = innerPrice - tax;
+    addTaxPrice.innerHTML = tax;
+    profit.innerHTML = pro;
+  });
+});

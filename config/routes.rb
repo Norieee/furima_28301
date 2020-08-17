22fd3c932cbd2ebb1405d45deba20dda6241a3f1
Norieee <aok76283@gmail.com>
2020-08-17 18:08:57 +0900

    購入画面へ遷移（ログインユーザーのみ）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   root "items#index"
-  resources :items, only: [:new, :create, :show, :destroy]
+  resources :items, only: [:new, :create, :show, :destroy] do
+    resources :ordered_items, only: :index
+  end
 end

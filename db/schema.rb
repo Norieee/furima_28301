27941ae06b2c34f6a45cd9a853f375bb7696fa1c
Norieee <aok76283@gmail.com>
2020-08-17 10:56:08 +0900

    カラム型の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_16_112056) do
+ActiveRecord::Schema.define(version: 2020_08_17_013502) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -51,8 +51,10 @@ ActiveRecord::Schema.define(version: 2020_08_16_112056) do
   create_table "ordered_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "item_id"
-    t.integer "user_id"
+    t.bigint "item_id"
+    t.bigint "user_id"
+    t.index ["item_id"], name: "index_ordered_items_on_item_id"
+    t.index ["user_id"], name: "index_ordered_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -75,4 +77,6 @@ ActiveRecord::Schema.define(version: 2020_08_16_112056) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "ordered_items", "items"
+  add_foreign_key "ordered_items", "users"
 end

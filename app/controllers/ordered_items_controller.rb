b9ae7ee4ba8a39402206e5616c3b927b3d88a390
Norieee <aok76283@gmail.com>
2020-08-20 18:56:51 +0900

    コード整備(before_action)・ページ遷移設定(URL直接入力時)

--- app/controllers/ordered_items_controller.rb
+++ app/controllers/ordered_items_controller.rb
@@ -1,16 +1,12 @@
 class OrderedItemsController < ApplicationController
-  before_action :move_to_new
-
-  def index
-    @item = Item.find(params[:item_id])
-  end
+  before_action :move_to_new, :move_to_top
+  before_action :set_item, only: [:index, :create]
 
   def new
     @itemAddress = ItemAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @itemAddress = ItemAddress.new(order_params)
     if @itemAddress.valid?
       pay_item
@@ -27,6 +23,13 @@ class OrderedItemsController < ApplicationController
     redirect_to controller: 'users/sessions', action: :new unless user_signed_in?
   end
 
+  def move_to_top
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user.id || @item.ordered_item != nil
+      redirect_to root_path
+    end
+  end
+
   def order_params
     params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id).merge(user_id: current_user.id)
   end
@@ -38,5 +41,9 @@ class OrderedItemsController < ApplicationController
       card: params[:token],
       currency:'jpy'
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end

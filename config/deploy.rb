aaaa8bf4a47d033252088fec89cb0d0fadfc3754
Norieee <aok76283@gmail.com>
2020-08-26 11:17:19 +0900

    誤記修正（deploy.rb）

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'furima_28301''
+set :application, 'furima_28301'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:Norieee/furima_28301.git'

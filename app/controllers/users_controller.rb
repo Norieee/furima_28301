8a43c2a2b3cae20be8804d3660a948364e8841e6
Norieee <aok76283@gmail.com>
2020-08-29 22:56:24 +0900

    マイページ（viewの調整）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,22 @@ class UsersController < ApplicationController
     @ordered_ids = @user_ordered.pluck(:item_id)
     @ordered_items = @items.where(id: @ordered_ids)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email)
+  end
 end
